d77b3574999fe774cacfad5c8c5038a593290eca
thoma <thomas.duhamel@mines-ales.org>
2022-11-21 14:40:15 +0100

    Node modules

--- backend/src/main/java/com/IA/Promo171/WikiHoax/backend/WikiScrapper.java
+++ backend/src/main/java/com/IA/Promo171/WikiHoax/backend/WikiScrapper.java
@@ -1,24 +1,27 @@
 package com.IA.Promo171.WikiHoax.backend;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.stereotype.Component;
 
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlImage;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
+
 public class WikiScrapper {
-    public static void main(String[] args) {
+
+    private static PageDao pageDao;
+
+    public static void main(String[] args){
         try {
             final WebClient client = new WebClient();
             client.getOptions().setCssEnabled(false);
             client.getOptions().setJavaScriptEnabled(false);
 
-            //String url="https://fr.wikihow.com/arrêter-de-baver-pendant-son-sommeil";
-            String url="https://fr.wikihow.com/persuader-les-gens-de-voter-pour-moi";
+            String url="https://fr.wikihow.com/arrêter-de-baver-pendant-son-sommeil";
 
             final HtmlPage page = client.getPage(url);
 
@@ -28,22 +31,33 @@ public class WikiScrapper {
 
             String desc=page.getElementById("mf-section-0").asNormalizedText();
             System.out.println(desc);
+    
+    
+
+            Page wikiPage=pageDao.newPage(new Page(title,desc));
+
+            Long id=wikiPage.getId();
 
             ArrayList<String> imgUrls=new ArrayList<>();
 
             List<HtmlImage> imgs=page.getByXPath("//div//div[@class='section_text']//ol[@class='steps_list_2']//li//div[@class='mwimg  largeimage  floatcenter ']//a[@class='image']//div[@class='content-spacer']//img");
 
-            Files.createDirectory(Paths.get("D:/WikiHoaxImages/"+title));
 
             for(HtmlImage img : imgs){
                 String imgUrl=img.getAttribute("data-src");
 
                 imgUrls.add(imgUrl);
+
+                Image im= new Image(imgUrl,wikiPage);
+
+                wikiPage.addImage(im);
             }
+
 
           }
           catch (Exception e) {
             e.printStackTrace();
           }
     }
+    
 }
