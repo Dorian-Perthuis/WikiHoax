321327f16cfd74d05841897bfd1d55f5312102d7
Adwelz <43895468+Adwelz@users.noreply.github.com>
2022-11-22 18:40:30 +0100

    addUserDao

---
+++ backend/src/main/java/com/IA/Promo171/WikiHoax/UserDao.java
@@ -0,0 +1,49 @@
+package com.IA.Promo171.WikiHoax;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.IA.Promo171.WikiHoax.backend.User;
+
+@Repository
+@Transactional
+public class UserDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public List<User> listUser() {
+        Session session = this.sessionFactory.getCurrentSession();
+        Query<User> query = session.createQuery("From User", User.class);
+        return query.getResultList();
+    }
+
+    public User newUser(User newUser) {
+        Session session = this.sessionFactory.getCurrentSession();
+        session.save(newUser);
+        return newUser;
+    }
+
+    public User getUser(Long id) {
+        Session session = this.sessionFactory.getCurrentSession();
+        Query<User> query = session.createQuery("From User user where user.id = :id", User.class).setParameter("id",
+                id);
+        return query.getSingleResult();
+    }
+
+    public String getUserMdp(String identifiant) {
+        Session session = this.sessionFactory.getCurrentSession();
+        Query<String> query = session
+                .createQuery("SELECT mdp From User user where user.identifiant = :identifiant", String.class)
+                .setParameter("identifiant",
+                        identifiant);
+        return query.getSingleResult();
+    }
+
+}
